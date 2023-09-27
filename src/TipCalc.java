711a7de39a05f9cebdd3b20e343f759fb1972fee
ninjastar779 <neeldebnath2@gmail.com>
2023-09-27 17:42:17 -0400

    Project

--- src/TipCalc.java
+++ src/TipCalc.java
@@ -18,10 +18,12 @@ public class TipCalc {
 
         double totalTip = totalBill * (tipPercentage / 100); //total tip
         double totalBillWithTip = totalBill + totalTip; //bill+tip
+        double tipPerPerson = totalTip / numberOfPeople; //tip amount per person
         double totalPerPerson = totalBillWithTip / numberOfPeople; //total per person
 
         System.out.printf("Total tip: $%.2f%n", totalTip);
         System.out.printf("Total bill including tip: $%.2f%n", totalBillWithTip);
+        System.out.printf("Tip per person $%.2f%n", tipPerPerson);
         System.out.printf("Total per person: $%.2f%n", totalPerPerson);
     }
 }
